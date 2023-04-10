13c0ed59b93c761be00702582af627bab2db14f5
Nesterov Roman <nester@bk.ru>
2023-04-10 22:09:39 +0300

    docs: add simple example

---
+++ examples/simple.rs
@@ -0,0 +1,28 @@
+use bullrs::queue::Queue;
+use bullrs::job::Job;
+use tokio::time::sleep;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("bulljs main");
+
+    let queue = Queue::new("queue", "redis://localhost:6379").await?;
+
+    let qr = queue.process(|data: String| async move {
+        sleep(Duration::from_millis(1000)).await;
+        println!("data: {}", data);
+        "yyyy".to_string()
+    }).await?;
+
+    dbg!(qr);
+
+    // queue.process("queue process").await?;
+    let job = Job::new("new job");
+
+    queue.add_job(job).await?;
+
+    sleep(Duration::from_secs(3)).await;
+
+    Ok(())
+}
